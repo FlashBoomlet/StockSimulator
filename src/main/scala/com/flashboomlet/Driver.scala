3d6d6ed9a612ab90e9469ba438ebd10b727e0d17
Lyncht <lyncht@unm.edu>
2016-10-07 18:17:56 -0600

    Set up

---
+++ src/main/scala/com/flashboomlet/Driver.scala
@@ -0,0 +1,11 @@
+package com.flashboomlet
+
+/**
+  * Created by ttlynch on 10/7/16.
+  */
+object Driver {
+
+  def main(args: Array[String]) {
+    println("We are up and functioning world")
+  }
+}
